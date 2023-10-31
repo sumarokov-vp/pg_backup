92360204781222649b5c0d92689154fdc508289b
sumarokov-vp <sumarokov.vp@gmail.com>
2023-11-01 04:05:23 +0600

    fix compress

--- backup.py
+++ backup.py
@@ -1,6 +1,7 @@
 import gzip
 import os
 from datetime import datetime
+from shutil import copyfileobj
 
 from dotenv import load_dotenv
 from sh import pg_dump
@@ -15,16 +16,26 @@ DATABASES = [
     "warninger",
 ]
 
-BACKUP_PATH = "/root/yandex_uploader/tempbackup/"
+BACKUP_PATH = os.getenv("PGBACKUP_PATH")
+TEMP_PATH = os.getenv("PGTEMP_PATH")
 
 
 def backup():
     load_dotenv()
     for db in DATABASES:
         date = datetime.now().strftime("%Y-%m-%d")
-        filename = os.path.join(BACKUP_PATH, f"{db}-{date}.sql")
-        with gzip.open(f"{filename}.gz", "wb") as f:
+        temp_filename = os.path.join(TEMP_PATH, f"{db}-{date}.sql")
+        filename = os.path.join(BACKUP_PATH, f"{db}-{date}.sql.gz")
+
+        # Dump database
+        with open(temp_filename, "wb") as f:
             pg_dump("-h", "localhost", db, _out=f)
+
+        # Compress file
+        with open(temp_filename, "rb") as f_in:
+            with gzip.open(filename, "wb") as f_out:
+                copyfileobj(f_in, f_out)
+        os.remove(temp_filename)
 
 
 if __name__ == "__main__":
