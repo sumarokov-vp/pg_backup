d1f6470b3c2d3fd6243693763c392498d8cbb80b
sumarokov-vp <sumarokov.vp@gmail.com>
2023-11-01 03:34:56 +0600

    init

---
+++ backup.py
@@ -0,0 +1,26 @@
+import gzip
+
+from sh import pg_dump
+
+DATABASES = [
+    "dm_vietnam",
+    "bot_cambo",
+    "bot_vietnam",
+    "dm_nigeria",
+    "dm_cambodia",
+    "bot_nigeria",
+    "warninger",
+]
+
+BACKUP_PATH = "/root/yandex_uploader/tempbackup/"
+
+
+def backup():
+    for db in DATABASES:
+        filename = os.path.join(BACKUP_PATH, f"{db}-{date}.sql")
+        with gzip.open(f"{filename}.gz", "wb") as f:
+            pg_dump("-h", "localhost", db, _out=f)
+
+
+if __name__ == "__main__":
+    backup()
