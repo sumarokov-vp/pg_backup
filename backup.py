d05c5ab2a2f99c249a2b7c105f3fb00bd3db5a51
sumarokov-vp <sumarokov.vp@gmail.com>
2023-11-01 04:06:41 +0600

    fix

--- backup.py
+++ backup.py
@@ -16,16 +16,15 @@ DATABASES = [
     "warninger",
 ]
 
-BACKUP_PATH = os.getenv("PGBACKUP_PATH")
-TEMP_PATH = os.getenv("PGTEMP_PATH")
-
 
 def backup():
     load_dotenv()
+    backup_path = os.getenv("PGBACKUP_PATH")
+    temp_path = os.getenv("PGTEMP_PATH")
     for db in DATABASES:
         date = datetime.now().strftime("%Y-%m-%d")
-        temp_filename = os.path.join(TEMP_PATH, f"{db}-{date}.sql")
-        filename = os.path.join(BACKUP_PATH, f"{db}-{date}.sql.gz")
+        temp_filename = os.path.join(temp_path, f"{db}-{date}.sql")
+        filename = os.path.join(backup_path, f"{db}-{date}.sql.gz")
 
         # Dump database
         with open(temp_filename, "wb") as f:
